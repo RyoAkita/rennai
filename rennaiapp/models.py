5820a58a6f2f7efd86ab976b6f760216c2aaae3b
SuzukiRikuto <riku2926@Rikutos-MacBook.local>
2019-07-14 12:02:55 +0900

    models.pyに追記

--- rennaiapp/models.py
+++ rennaiapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class BoardModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    images = models.ImageField(upload_to='')
+    good = models.IntegerField()
+    bad = models.IntegerField()
+    comments = models.TextField()
